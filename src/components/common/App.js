2a6ad6f9bfd889daa4dd53be0f0f336c99834723
Antonije <antonijek@yahoo.com>
2023-02-01 11:40:54 +0100

    dropdown menu

--- src/components/common/App.js
+++ src/components/common/App.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import "../../styles/app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BasicInformation from "../user/basicInformation";
@@ -9,47 +9,38 @@ import Login from "../admin/login";
 import Admin from "../admin/admin";
 import AnketList from "../user/anketList";
 import Statistics from "../admin/statistics";
-import { getData } from "../../api";
 import Profile from "../admin/profile";
+import Test from "../user/test";
 
 let token = localStorage.getItem("token");
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [lastResearch, setLastResearch] = useState("");
-
-  const getAllQuestionnaires = async () => {
-    const res = await getData(token);
-    setData(res.data);
-    setLastResearch(res.data[res.data.length - 1]);
-  };
-
-  useEffect(() => {
-    getAllQuestionnaires();
-  }, []);
-
+  const [token1, setToken1] = useState("");
+  console.log(token1);
   return (
     <BrowserRouter>
-      <Header />
+      <Header token1={token1} setToken1={setToken1} />
 
       <div className="app">
         <Routes>
           {token !== null ? (
-            <Route
-              exact
-              path="/"
-              element={<Questionnaire data={lastResearch} />}
-            />
+            <Route exact path="/" element={<Questionnaire />} />
           ) : (
             <Route exact path="/" element={<BasicInformation />} />
           )}
-
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setToken1={setToken1} />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/ankets" element={<AnketList />} />
-          <Route exact path="/admin/research" element={<Admin />} />
+          <Route
+            exact
+            path="/admin/research"
+            element={<Admin token1={token1} />}
+          />
           <Route exact path="/statistics" element={<Statistics />} />
-          <Route path="/admin/research/questionnaire/:id" element={<Admin />} />
+          <Route
+            path="/admin/research/questionnaire/:id"
+            element={<Admin token1={token1} />}
+          />
         </Routes>
       </div>
       <Footer />
